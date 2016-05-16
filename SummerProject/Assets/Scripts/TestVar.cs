ee89e6370c5ef68a027a1ebdd07a04f5b1e8596e
Fritz Dittmer <Fdittmer9762@gmail.com>
2016-05-16 11:37:15 -0600

    Var Scripting
    making a variable script

---
+++ SummerProject/Assets/Scripts/TestVar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestVar : MonoBehaviour {
+
+	public int cookies = 12;
+	int portion;
+	int kids;
+	int leftovers;
+
+	void Start () 
+	{
+		kids = Random.Range (1, 4);
+		portion = Random.Range (1, 3);
+		Cookies (leftovers);
+		Debug.Log (leftovers);
+	}
+
+	int Cookies (int numberEaten)
+	{
+		numberEaten = kids * portion;
+		leftovers = cookies - numberEaten;
+		return leftovers;
+	}
+}
