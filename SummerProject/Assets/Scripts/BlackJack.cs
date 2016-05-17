f97f9415b66f66bddcb2129fb09ec5b974028c21
Fritz Dittmer <fdittmer9762@gmail.com>
2016-05-17 12:32:27 -0600

    BackJack Start
    *creating the Black Jack script
    *creating the draw card function

---
+++ SummerProject/Assets/Scripts/BlackJack.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlackJack : MonoBehaviour 
+{
+	int card;
+	int myHand;
+	//int dealerHand;
+
+	void Awake ()
+	{
+		myHand = 0;
+		//dealerHand = 0;
+	}
+
+	void Start ()
+	{
+		Draw ();
+		Debug.Log (myHand);
+	}
+
+	int Draw ()
+	{
+		card = Random.Range (1, 13);
+		myHand += card;
+		return myHand;
+	}
+}
