81f5fd0e1f03494df2deeb3558c13baa1fdd8614
Fritz Dittmer <fdittmer9762@gmail.com>
2016-05-17 12:56:19 -0600

    Bust function
    *comparing the value of cards
    (not the best way to do it, taking a break to see if there is a better way to return the bools)

--- SummerProject/Assets/Scripts/BlackJack.cs
+++ SummerProject/Assets/Scripts/BlackJack.cs
@@ -6,6 +6,8 @@ public class BlackJack : MonoBehaviour
 	int card;
 	int myHand;
 	//int dealerHand;
+	bool blackJack;
+	bool bust;
 
 	void Awake ()
 	{
@@ -17,8 +19,29 @@ public class BlackJack : MonoBehaviour
 	{
 		Draw ();
 		Debug.Log (myHand);
+		Table ();
+		Debug.Log (bust);
 	}
 
+	void Table ()
+	{
+		blackJack = false;
+		if (myHand >= 22) 
+		{
+			bust = true;
+		}
+		else if (myHand <= 20) 
+		{
+			bust = false;
+		} 
+		else 
+		{
+			blackJack = true;
+			bust = true;
+		}
+
+	}
+		
 	int Draw ()
 	{
 		card = Random.Range (1, 13);
