2ccc36249c0f31f6a11f1b5cfe897b641f93611b
Fritz Dittmer <fdittmer9762@gmail.com>
2016-05-23 10:59:50 -0600

    Syntax Lesson
    adding  the syntax script

---
+++ SummerProject/Assets/Scripts/Syntax.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Syntax : MonoBehaviour
+{
+	
+	public int myInt;
+	//public int yourInt;
+
+
+	int MyInt (int _i, int _j) //it looks like the underscore is just part of the name. Things can be placed in front of and behind the underscore and it does not affect the int.
+	{
+		return _i - _j;
+	}
+
+
+	void Start ()
+	{
+		//myInt = MyInt (20,8);
+		//print (myInt);
+		 print (MyInt (myInt, 8));
+		// would this be easier?
+	}
+
+
+}
