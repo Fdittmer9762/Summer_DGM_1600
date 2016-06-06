84e470fd0a3b90c522f7a4e8a4b070c6a5d0704e
Fritz Dittmer <fdittmer9762@gmail.com>
2016-06-06 11:30:12 -0600

    If statements
    if statement class assignment

---
+++ SummerProject/Assets/Scripts/IfStatements.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfStatements : MonoBehaviour {
+
+	public bool myBool = true;
+	public int myInt = 1;
+	public string myString;
+	const string myConst = "password";
+
+	void Start () {
+		if (myBool == true) {
+			print ("Open The Gates");
+		} 
+		else {
+			print ("Close The Gates");
+		}
+	}
+
+	void Update () {
+		// not a good idea to put a if statement in update, takes a tonne of processing power
+		// unless you're teaching
+		if (myString== myConst) {
+			print ("That's correct, you have unlocked things");
+		}
+		//this will check endlessly, reguardless if it prints
+		else {
+			print ("Nice try");
+		}
+	}
+}
