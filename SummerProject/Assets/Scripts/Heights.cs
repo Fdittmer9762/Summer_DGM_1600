3161f27791b5772e5e595c4c9ced4a11b9e08185
Fritz Dittmer <fdittmer9762@gmail.com>
2016-06-08 12:23:07 -0600

    Height Script
    made a script that sets and comp heights of two people
    ** still need to work out issues with setting height**

---
+++ SummerProject/Assets/Scripts/Heights.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Heights : MonoBehaviour {
+
+	public float sarah;
+	public float emma;
+
+
+	void Start () {
+		SetHeight (sarah);
+		SetHeight (emma);
+		WhoIsTaller ();
+	}
+
+	float SetHeight (float height){
+		height = Random.Range (4, 6); //can't figure out why it won't change their values
+		return height;
+	}
+
+	void WhoIsTaller (){
+		if (sarah > emma) {
+			print ("Sarah is taller.");
+		}
+		if (sarah < emma) {
+			print ("Emma is taller.");
+		} else {
+			print ("They are the same.");
+		}
+	}
+}
