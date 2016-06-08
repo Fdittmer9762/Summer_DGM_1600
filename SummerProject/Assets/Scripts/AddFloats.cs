885bbcb810eecea62fbb86bda4a8a7748e247c32
Fritz Dittmer <fdittmer9762@gmail.com>
2016-06-08 10:27:56 -0600

    Comp Test
    pop quiz to create simple script

---
+++ SummerProject/Assets/Scripts/AddFloats.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddFloats : MonoBehaviour {
+
+	float A = 20;
+	float B = 15;
+
+	void Start () {
+		print (Addition (A,B));
+	}
+
+	float Addition (float x, float y){
+		float z = x + y;
+		return z;
+	}
+}
