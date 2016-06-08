521485c67df3a4de487d86973902d18145fe8566
Fritz Dittmer <fdittmer9762@gmail.com>
2016-06-08 11:45:09 -0600

    Coin Toss Scripts
    an if statement and a switch statement script

---
+++ SummerProject/Assets/Scripts/CoinToss.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinToss : MonoBehaviour {
+
+	string user1 = "Player 1";
+	string user2 = "Player 2";
+	//public int coin;
+
+	public bool winnerBool;
+
+	void FixedUpdate () {
+		TestWhoWon ();
+	}
+
+	void TestWhoWon (){
+		if (winnerBool) {
+			print (user1 + "is the winner");
+		}
+		else {
+			print (user2 + "is the winner");
+		}
+	}
+}
---
+++ SummerProject/Assets/Scripts/NewCoinToss.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewCoinToss : MonoBehaviour {
+
+
+	public int coinToss = 2;
+
+	void FixedUpdate () {
+		coinToss = Random.Range (1, 3);
+		Call (coinToss);
+	}
+
+	void Call (int toss) {
+		switch (toss)
+		{
+		case 2:
+			print ("Heads");
+			break;
+		case 1:
+			print ("Tails");
+			break;
+		}
+	}
+}
