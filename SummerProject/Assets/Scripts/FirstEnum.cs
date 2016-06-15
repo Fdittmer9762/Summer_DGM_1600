1f2dccdbd49238ebb1d780f4ed14ea9b222b997b
Fritz Dittmer <fdittmer9762@gmail.com>
2016-06-15 11:01:25 -0600

    First Enum Script
    creating an enum script

---
+++ SummerProject/Assets/Scripts/FirstEnum.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstEnum : MonoBehaviour {
+
+	public enum Operations { ADD, SUBTRACT, MULTIPLY, DIVIDE, SQUARE };
+
+	public Operations currentOperation;
+
+	public int numOne;
+	public int numTwo;
+
+	int ChangeOperation (int x, int y){
+		int value = 0;
+		switch (currentOperation) {
+		case Operations.ADD:
+			value = x + y;
+			break;
+		case Operations.SUBTRACT:
+			value = x - y;
+			break;
+		case Operations.MULTIPLY:
+			value = x * y;
+			break;
+		case Operations.DIVIDE:
+			value = x / y;
+			break;
+		case Operations.SQUARE:
+			value = (x+y)*(x+y);
+			break;
+		default:
+			Debug.Log ("error");
+			break;
+		}
+		return value;
+	}
+
+	void OnMouseDown (){
+		int finalValue = ChangeOperation (numOne, numTwo);
+		print (finalValue);
+	}
+}
