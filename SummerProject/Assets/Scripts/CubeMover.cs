d9b50022628ac547a1c163c426767a5d05b38ba7
Fritz Dittmer <fdittmer9762@gmail.com>
2016-06-15 11:42:48 -0600

    Moving Cube
    creating scripts to move a cube

---
+++ SummerProject/Assets/Scripts/CubeMover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeMover : MonoBehaviour {
+
+	public float xDistance = 0;
+	public Vector3 pos;
+
+	//this is the best way to do this, is uses much less memory and stops when it reaches the end
+	//use enumerators and loops whenever possible, or when you need things to stop
+	IEnumerator MoveInX () {
+		while (pos.x <= xDistance) {
+			pos.x += .1f;
+			transform.position = pos;
+			print (pos.x);
+			yield return new WaitForSeconds (0.1f);
+		}
+	}
+
+	void OnMouseDown () {
+		xDistance += 1;
+		StartCoroutine (MoveInX ());
+	}
+}
---
+++ SummerProject/Assets/Scripts/MoveOnUpdate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveOnUpdate : MonoBehaviour {
+
+	//this is bad, update never stops
+
+	void Update () {
+		if (transform.position.x <= 10)
+			transform.Translate (0.1f, 0, 0);
+
+		print ("update");
+	}
+}
