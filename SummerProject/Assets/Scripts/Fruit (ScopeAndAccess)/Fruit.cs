bc4e7efab867fe850a13999b4cb3766dbc2d701c
Fritz Dittmer <fdittmer9762@gmail.com>
2016-06-20 11:28:38 -0600

    Scope and Access
    fruit scripts for scope and access

---
+++ SummerProject/Assets/Scripts/Fruit (ScopeAndAccess)/Fruit.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fruit { //monodevelop must be removed in order to access anything from the script without being instanciated
+
+	protected int apples;
+	private int bananas;
+	public int lemons; //this will be overriden by anything within a function;
+
+	public void AddApples (int moreApples){
+		apples += moreApples; //more apples can only be accessed by the AddApples script
+	}
+
+	public void AddBananas (int moreBananas){
+		bananas += moreBananas; //more bananas can only be accessed by the AddBAnanas script
+	}
+
+	public int MixedFruit (){
+		return apples + bananas + lemons;
+	}
+
+	private void EatFruit (int portion){
+		apples-= portion;
+		bananas-= portion;
+		lemons-= portion;
+	}
+}
---
+++ SummerProject/Assets/Scripts/Fruit (ScopeAndAccess)/UseFruit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UseFruit : MonoBehaviour { //monobehaviour makes it an instance of the script and must be instanciated before it can be accessed
+
+	private Fruit MyFruit;
+
+	void Start (){
+		MyFruit = new Fruit (); //this can be done within or outside of the function
+		MyFruit.AddApples (10);
+		MyFruit.AddBananas (5);
+		MyFruit.lemons = 6;
+		int allFruit = MyFruit.MixedFruit ();
+		print (allFruit);
+	}
+
+
+}
