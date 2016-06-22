33b50666c403e750e0936d97acdd93b21a69458b
Fritz Dittmer <fdittmer9762@gmail.com>
2016-06-22 12:44:20 -0600

    MovePlayer Script
    added move player script

---
+++ SummerProject/Assets/Scripts/UserInput/MovePlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+
+	Vector3 pos;
+	public float speed = 10;
+
+	/*public float speed {
+		get;
+		set;
+	}*/
+
+	void Move ()
+	{
+		//move character in X using the arrow keys
+		pos.x = Input.GetAxis ("Horizontal")* Time.deltaTime* speed;
+		transform.Translate (pos);
+	}
+
+	void Update () {
+		Move ();
+	}
+}
